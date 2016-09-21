e0ba3ec7d8869b4f29a445ee0cb3d86c87907863
zangi <zangi@zangis-iMac.fios-router.home>
2016-09-21 10:04:42 -0400

    commiting js and album html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,48 @@
-console.log('This would be the main JS file.');
+
+
+
+function Album(name,songList,artist,isFavorite){
+	this.name=name;
+	this.artist=artist;
+	this.isFavorite=isFavorite;
+	this.songList=new Array();
+	for(var i=0;i<songList.length;i++){
+		this.songList[i]=songList[i];
+	}
+}
+Album.prototype.setFavorite=function(isFavorite){
+	this.isFavorite=isFavorite;
+}
+
+Album.prototype.getName=function(){
+	return this.name;
+}
+Album.prototype.getArtist=function(){
+	return this.artist;
+}
+
+Album.prototype.getSongList=function(){
+	return this.songList;
+}
+
+var redAlbum = new Album("Red",["State of Grace","Red","Treacherous"],"Taylor Swift".0);
+var myEverythingAlbum = new Album("My Everything",["Problem","One Last","The Weekend"],"Ariana Grande",0);
+var aHeadFullOfDreamsAlbum = new Album("A head full of dreams",["A head full of dreams","Birds","Hymn for weekend"],"Coldplay",1
+	);
+
+//event handling
+document.addEventListener("DOMContentLoader",function(event){
+		function printAlbumList(event){
+			//create a list
+			//bind it to the main content classfor display
+			//the event will be load
+			function printSongs(event){
+				var songLine = "<h2>state of red</h2>";
+				document.getElementById("album-content").innerHTML=songLine;
+			}
+			document.querySelector("#red").addEventListener("click",printSongs);
+		}
+
+	}
+);
+
